be4c09834779042246006f4551b89e95b167beaf
aghassaei <aghassaei@reed.edu>
2021-12-13 21:27:04 -0800

    pushing

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 #main program
-# make a new git, read in everything
-## ajsdfhasdufalskdjfbalwubfiowbciqbapwejfbpwefbpjsdbLDSFJWLEOUFBpjB
+# remove duplicates in all triangles, figure this out
+
 from utils import*
 def main():
     # Read in example
     node_list, edge_list= read_in('example_edges.txt')
     if verbose_read:
         print(node_list)
-        print(edge_list)
-    
+        print(edge_list)    
     
     assert(len(edge_list) == num_edges)
     assert(len(node_list) == num_nodes)
@@ -16,17 +15,20 @@ def main():
     # Make adjacency list
     adj_list = edge_to_adj(edge_list)
     assert(len(adj_list) == num_nodes)
-    if verbose_adj:
-        print(adj_list)
+    if verbose_adj: print(adj_list)
     
-    nodes_triangles = get_triangles_for('u0', adj_list)
-    if verbose_tri:
-        print(nodes_triangles)
-
-    # make function that takes a single node and returns list of tuples with other
-    # nodes in that triangle
-    # make function that does this for all nodes
-        # return that 
+    #get pruned adj list
+    
+    
+    # Get node labels
+    node_info, labeled_nodes, pos_nodes, neg_nodes = read_labeled_nodes_example()
+    
+    all_triangles = remove_duplicates(get_all_triangles(node_list, adj_list))
+    if verbose_tri: print('all triangles:\n'+str(all_triangles)+'\n')    
+    
+    triangles_dict = get_triangle_dictionary(all_triangles, pos_nodes, neg_nodes, all_keys)
+    #assert(len(triangle_dict.keys() == ?))
+    if verbose_tri_dict: print('triangles_dict = \n'+str(triangles_dict) +'\n')
     return 0
 
 
@@ -36,6 +38,10 @@ num_nodes = 10
 verbose_read = False
 verbose_adj = False
 verbose_tri = True
+verbose_tri_dict = True
+
+all_keys = ['pos_pos_pos_', 'pos_pos_neg_', 'pos_neg_neg_', 'neg_neg_neg_'
+            'pos_pos_u', 'pos_u_u_', 'neg_neg_u_', 'neg_u_u_', 'u_u_u_', 'neg_pos_u_']
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -24,7 +24,7 @@ def read_in(filename):
     file.close()
     text = text.split()
     
-    if text[0] == '#': del text[0:2] #removes column headers from text
+    if text[0] == '#Name' or text[0] == '#': del text[0:2] #removes column headers from text
                    
     edge_list = [] #initialize edge list, node list
     node_list = []                       
@@ -70,7 +70,43 @@ def edge_to_adj(edge_list):
 
     return(adjacency_list)
 
+def remove_duplicates(all_triangles):
+    
+    # Set so that it's easier to look up
+    already_seen = {}
+    for triangle in all_triangles:
+        all_permutations = {}
+        for perm in all_permutations:
+            if perm not in already_seen:
+                already_seen += {perm}
+    return list(already_seen)
 
+def get_all_triangles(node_list, adj_list):
+    all_triangles = []
+    for node in node_list:
+        all_triangles += get_triangles_for(node, adj_list)
+    return all_triangles
+
+### get master triangle dictionary
+# uses all the triangles
+
+def get_triangle_dictionary(all_triangles, pos_nodes, neg_nodes, all_keys):
+    # Initialize an empty dictionary with all possible combinations of labels
+    triangles_dict = {}
+    for key in all_keys:
+        triangles_dict[key] = []
+    print(triangles_dict)
+    print('\n')
+    print('len triangles_dict = '+str(len(triangles_dict)))
+    print('len all keys = '+str(len(all_keys)))
+    assert(len(triangles_dict) == len(all_keys))
+    for triangle in all_triangles:
+        print('current triangle in get_triangle_dict = '+str(triangle))
+        assert(len(triangle) == 3)
+        key = get_key(get_tag(triangle, pos_nodes, neg_nodes))
+        triangles_dict[key] += [triangle]
+    return triangles_dict
+        
 #make sets to make easier
 def get_triangles_for(node, adj_list):
     nodes_triangles = []
@@ -81,7 +117,8 @@ def get_triangles_for(node, adj_list):
             if u in adj_list[v] and (v, u) not in nodes_triangles:
                 #print(u, v)
                 #print('\n')
-                nodes_triangles += [(u, v)]
+                nodes_triangles += [(node, u, v)]
+                #(?) not appending correctly
             #else: print("not triangle")
     return nodes_triangles
 
@@ -92,15 +129,60 @@ def get_node_label(node, pos_nodes, neg_nodes):
         label = 'neg'
     else:
         label = 'u'
+        print('unlabeled node = '+str(node))
     return label
 
-#triangle is a tuple or 3
-
-def caetgorize(triangle, pos_nodes, neg_nodes, u_nodes):
+def get_tag(triangle, pos_nodes, neg_nodes):
+    assert(len(triangle) == 3)
     tag = ''    
     for node in triangle:
-        label = get_node_label(node)
-        tag+= label
+        label = get_node_label(node, pos_nodes, neg_nodes)
+        tag+= label +str('_')
+    return get_key(tag)
+
+def get_key(tag):
+    
+    # If all nodes are positive
+    if tag == 'pos_pos_pos_':
+        return tag
+    
+    # If two positive, one negative
+    elif tag == 'pos_pos_neg_' or tag == 'pos_neg_pos_' or tag == 'neg_pos_pos_':
+        return 'pos_pos_neg_'
+     
+    # If one positive, two negative
+    elif tag == 'pos_neg_neg_' or tag == 'neg_neg_pos_' or tag == 'neg_pos_neg_':
+        return 'pos_neg_neg_'
+    
+    # If all negative
+    elif tag == 'neg_neg_neg_':
+        return tag
+        
+    # If two positive, one unlabeled
+    elif tag == 'pos_pos_u_' or tag == 'pos_u_pos_' or tag == 'u_pos_pos_':
+        return 'pos_pos_u'
+        
+    # If one positive, two unlabeled
+    elif tag == 'pos_u_u_' or tag == 'u_pos_u_' or tag == 'u_u_pos_':
+        return 'pos_u_u_'
+       
+            
+    # If two negative, one unlabeled
+    elif tag == 'neg_neg_u_' or tag == 'neg_u_neg_' or tag == 'u_neg_neg_':
+        return 'neg_neg_u_'
+    
+    # If one negative, two unlabeled
+    elif tag == 'neg_u_u_' or tag == 'u_neg_u_' or tag == 'u_u_neg_':
+        return 'neg_u_u_'
+    
+    # If all unlabeled
+    elif tag == 'u_u_u_':
+        return tag
+    # If one of each
+    elif tag == 'neg_pos_u_' or tag == 'neg_u_pos_' or tag == 'pos_u_neg_' or tag == 'pos_neg_u_' or tag == 'u_pos_neg_' or tag == 'u_neg_pos_':
+        return 'neg_pos_u_'
+    else: print('exception = ' +tag)
+
     return tag
 
 # author: ingrid zoll
@@ -133,34 +215,25 @@ def read_labeled_nodes():
 
     return node_info, labeled_nodes, pos_nodes, neg_nodes
 
-
-
-# author: ingrid zoll, modified from
-# input: none. it automatically gets the labeled nodes file when it is run
-# outputs: has 4 different outputs
-## node_info: dictionary, with format {node: [id, pos_neg, annotation], etc}
-## labeled_nodes: list of all nodes in the labeled-nodes file
-## pos_nodes and neg_nodes: lists of positive and negative labeled nodes, respectively.
-# code to run: node_info, labeled_nodes, pos_nodes, neg_nodes = utils.read_labeled_nodes()
-# NOTE: MUST BE RUN IN YOUR OWN DIRECTORY.
-def read_labeled_nodes():
+def read_labeled_nodes_example():
     posneg_file = 'example_labels.txt'
     # initializing dictionary and lists
-
+    node_info = {}
     labeled_nodes = []
     pos_nodes = []
     neg_nodes = []
 
     with open(posneg_file) as fin:
         for line in fin: # for each line
-            n, ID, pos_neg, annotation = line.strip().split() # separate the four items
+            n, pos_neg = line.strip().split() # separate the two items
             if n != "#Name": # if the line is not the header (in this case, the header starts with "#Name")
                 if n not in labeled_nodes: # if the node is not a duplicate
                     labeled_nodes.append(n) # add it to the full node list
-                    node_info[n] = [ID, pos_neg, annotation] # add info to full node dictionary
+                    node_info[n] = [pos_neg] # add info to full node dictionary
                     if pos_neg == "Positive": # if the node is positive, add to positive list
                         pos_nodes.append(n)
                     elif pos_neg == "Negative": # if node is negative, add to negative list
                         neg_nodes.append(n)
 
     return node_info, labeled_nodes, pos_nodes, neg_nodes
+
